69ad509687792901d28729a8f0cf1fc7fe1132c3
TrunKumar <trun.kumar@innobitsystems.com>
2023-03-09 11:50:52 +0530

    Updated Factory Tags Scritp

--- factory-tag-migration-03-03-2023.js
+++ factory-tag-migration-03-03-2023.js
@@ -1,22 +1,19 @@
 const mongoose = require("mongoose");
 
 // for old production user
-var oldProd = mongoose.createConnection("");
+var oldProd = mongoose.createConnection("mongodb://localhost:27017/currentProduction");
 // for new production user
-var newProd = mongoose.createConnection("");
-
+var newProd = mongoose.createConnection("mongodb://localhost:27017/smartcosmos_prod");
 
 // creating customer HashMap
 async function getCustomerHashMap(connection, batchId) {
     let customerHashMap = new Map();
     let restData = await connection.collection('CustomerBatch').find({ "batchId": batchId }).toArray();
     for (const data of restData) {
-
         // getting customer details
         let customer = await connection.collection('Customer').find({ "customerId": data.customerId }).toArray();
-
         let batchObject = {
-            customerName: customer.customerName,
+            customerName: customer[0].customerName,
             deliveryDate: data.deliveryDate,
             deliveryItemName: data.deliveryItemName,
             deliveryQuantity: data.deliveryQuantity,
@@ -28,11 +25,12 @@ async function getCustomerHashMap(connection, batchId) {
             orderQuantityUnit: data.orderQuantityUnit,
         }
         customerHashMap.set(batchId, batchObject);
+       
     }
+    return customerHashMap;
 
 }
-
-
+    
 async function readTagData(oldProdConnection, newProdConnection) {
 
     console.log("Ready To Import Data from Current Production environement to New Production environment")
@@ -40,16 +38,21 @@ async function readTagData(oldProdConnection, newProdConnection) {
     limit = 5000;
     let chunkSize = 500;
     // getting distinct batch id
-    let batchData_ = await oldProdConnection.collection('Tag').distinct("bId").toArray();
-    for (const batchData of batchData_) {
+    let batchData_ = await oldProdConnection.collection('Tag').distinct("bId");
+    console.log("batchData_",batchData_);
+    for (let i=0;i<batchData_.length;i++) {
+        console.log("New Batch ID :",batchData_[i]);
         let bulkData = [];
         let offset = 0;
         //calling batchId HashMap
-        let custumerData = await getCustomerHashMap(oldProdConnection, batchData.htId)
+        let custumerData_ = await getCustomerHashMap(oldProdConnection, batchData_[i])
+        let custumerData=custumerData_.get(batchData_[i])
+        console.log(custumerData);
+    
         while (true) {
             let localCounter = 0;
-            let data_ = await oldProdConnection.collection('Tag').find({ "bId": batchData.htId }).sort({ ct: 1 }).skip(offset).limit(limit).toArray();
-            console.log("Total Record Found For BatchID=>",batchData.htId , "Using Offest ", offset, "Data Found==>",data_.length )
+            let data_ = await oldProdConnection.collection('Tag').find({ "bId": batchData_[i] }).sort({ ct: 1 }).skip(offset).limit(limit).toArray();
+            console.log("Total Record Found For BatchID=>",batchData_[i], "Using Offest ", offset, "Data Found==>",data_.length )
             if(data_.length===0)
             {
                 break;
@@ -58,43 +61,50 @@ async function readTagData(oldProdConnection, newProdConnection) {
             {
                 let final_data =
                 {
-                    tagId: batchData.htId,
+                    batchId:batchData_[i],
+                    tagId: data.htId,
                     secureKey: data.skey,
                     src: data.src,
-                    customerName: custumerData.customerName,
-                    deliveryDate: custumerData.deliveryDate,
-                    deliveryItemName: custumerData.deliveryItemName,
-                    deliveryQuantity: custumerData.deliveryQuantity,
-                    deliveryQuantityUnit: custumerData.deliveryQuantityUnit,
-                    historyReferenceId: custumerData.refOrderItemId,
-                    orderId: custumerData.orderId,
-                    orderDate: custumerData.orderDate,
-                    orderQuantity: custumerData.orderQuantity,
-                    orderQuantityUnit: custumerData.orderQuantityUnit,
+                    customerName: custumerData?.customerName,
+                    deliveryDate: custumerData?.deliveryDate,
+                    deliveryItemName: custumerData?.deliveryItemName,
+                    deliveryQuantity: custumerData?.deliveryQuantity,
+                    deliveryQuantityUnit: custumerData?.deliveryQuantityUnit,
+                    historyReferenceId: custumerData?.refOrderItemId,
+                    orderId: custumerData?.orderId,
+                    orderDate: custumerData?.orderDate,
+                    orderQuantity: custumerData?.orderQuantity,
+                    orderQuantityUnit: custumerData?.orderQuantityUnit,
                 }
+              
                 //  5100
                 // 0 < 500 && 5100 - 5000 > 500
                 if (bulkData.length < chunkSize && data_.length - localCounter > chunkSize) {
-                    bulkData.push(lastData);
+                    bulkData.push(final_data);
+                    counter++;
+                    localCounter++
                     continue;
+                    
                 }
-                localCounter++
-                counter++;
+               
                 bulkData.push(final_data);
                 // insert into new production DB
                 await newProdConnection.collection('unassignedTagsData').insertMany(bulkData);
                 bulkData = [];
+                counter++;
+                localCounter++
                 await delay(1000);
             }
             offset=offset+data_.length;
-            console.log("Total Inserted Records For Batch Id=> ",batchData.htId, "LocalCounter===>",localCounter," Total Inserted Records==>",counter);
-            await delay(2000);
+            console.log("Total Inserted Records For Batch Id=> ",batchData_[i], "LocalCounter===>",localCounter," Total Inserted Records==>",counter);
+            await delay(1000);
 
         }
     }
 
 
 }
+
 
 // for custum delay
 const delay = (delayInms) => {
@@ -103,5 +113,7 @@ const delay = (delayInms) => {
 
 }
 
+// calling main function
+readTagData(oldProd, newProd);
 
 
